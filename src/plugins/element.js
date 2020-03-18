c2eeb2ac9b74f3b91d2496fe6bd1422892552b6a
1372101837@qq.com <1372101837@qq.com>
2020-03-18 20:37:15 +0800

    src/router/index.js

---
+++ src/plugins/element.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Option } from 'element-ui'
+// 样式文件，需单独引入
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Option)
